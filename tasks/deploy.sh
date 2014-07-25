55efc61e1c075bf94cd1adf309c2a7d151dfc961
jahla <jahla@jahla.org>
2014-07-25 19:38:45 +1000

    Post update and un-publish

--- tasks/deploy.sh
+++ tasks/deploy.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-jekyll build && rsync -avz -e "ssh -p 2683 -i ~/.ssh/scarlett_rsa" --delete _site/ jahlaorg@jahla.org:public_html/
+jekyll build && rsync -vv -avz -e "ssh -p 2683 -i ~/.ssh/scarlett_rsa" --delete _site/ jahlaorg@jahla.org:public_html/
